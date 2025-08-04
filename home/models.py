fecd6cb35acf4ba4f03d4a40c85db9f09b0be42c
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-08-04 22:10:44 +0545

    basic model setup

--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class UserAccount(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user_image = models.ImageField(upload_to='user/image/',blank=True)
+
+
+class UserChat(models.Model):
+     user = models.ForeignKey(User,on_delete=models.CASCADE)
+     user_chat = models.TextField()
+     ai_reply = models.TextField()
+
+     class Meta:
+          verbose_name = 'conversation history'
+
+     def __str__(self):
+      return self.user.username
+     
