78e58bf2c54af1f6d8d6ccf70acd1ef1ef4516b6
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-08-07 20:56:02 +0545

    added new open ai model

--- home/views.py
+++ home/views.py
@@ -8,6 +8,98 @@ from . import models
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
 
+import base64
+import os
+from google import genai
+from google.genai import types
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def generate(text):
+#     client = genai.Client(
+#         api_key=os.environ.get("GEMINI_API_KEY"),
+#     )
+
+#     model = "gemini-2.5-pro"
+#     contents = [
+#         types.Content(
+#             role="user",
+#             parts=[
+#                 types.Part.from_text(text="""a py code to print helo world 
+# """),
+#             ],
+#         ),
+#         types.Content(
+#             role="model",
+#             parts=[
+#                 types.Part.from_text(text="""**Generating Python Snippet**
+
+# I'm focusing on crafting a concise Python code snippet. My primary goal is to fulfill the request for a \"hello world\" print statement. The simplicity of this task is at the forefront of my efforts.
+
+
+# **Creating Direct Output**
+
+# Now, I'm confident in delivering the Python code. I've distilled the request to its core: a \"hello world\" print statement. The simplicity of this task demands a direct, efficient approach. I've assessed the basic Python syntax and the core requirements of the prompt and am ready. The solution is print(\"hello world\").
+
+
+# """),
+#                 types.Part.from_text(text="""Of course, here is the Python code to print \"Hello, World!\":
+
+# ```python
+# print(\"Hello, World!\")
+# ```
+
+# ### How it works:
+
+# *   `print()` is a built-in Python function that displays output to the screen.
+# *   The text you want to print, in this case, \"Hello, World!\", is a string. In Python, strings are enclosed in either single or double quotation marks.
+
+# When you run this code, the message \"Hello, World!\" will be displayed on the screen."""),
+#             ],
+#         ),
+#         types.Content(
+#             role="user",
+#             parts=[
+#                 types.Part.from_text(text=text),
+#             ],
+#         ),
+#     ]
+#     tools = [
+#         types.Tool(googleSearch=types.GoogleSearch(
+#         )),
+#     ]
+#     generate_content_config = types.GenerateContentConfig(
+#         thinking_config = types.ThinkingConfig(
+#             thinking_budget=-1,
+#         ),
+#         tools=tools,
+#     )
+
+#     for chunk in client.models.generate_content_stream(
+#         model=model,
+#         contents=contents,
+#         config=generate_content_config,
+#     ):
+#         print(chunk.text, end="")
+
+# if __name__ == "__main__":
+    # generate()
+
+
+
+
 
 
 client = Groq(api_key=config('GROQ_API'))
@@ -26,22 +118,24 @@ def chat(request):
 def generate(text):
     full_response = ""
     completion = client.chat.completions.create(
-     model="gemma2-9b-it",
+     model="openai/gpt-oss-120b", 
      messages=[
         {
          "role": "system",
-         "content": "you are an advance ai named SKY !"
+         "content": "you are an advance ai named SKY ! made by Shishir Khatri also known as MRKC do not say repetly that u are SKy made by shishi Khatri aka MRKC"
        },
        {
          "role": "user",
          "content": text
        }
      ],
-     temperature=1,
-     max_completion_tokens=1024,
-     top_p=1,
-     stream=True,
-     stop=None,
+    
+    temperature=1,
+    max_completion_tokens=8192,
+    top_p=1,
+    reasoning_effort="medium",
+    stream=True,
+    stop=None
  )
 
     for chunk in completion:
